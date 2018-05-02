c4262913e42dbb2cf68295fdd085e5de808e9024
Prabeen04 <prabeen.strange@gmail.com>
2018-05-03 01:07:44 +0530

    Fab added

--- components/post/post.js
+++ components/post/post.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+import { Container, Header, Content, Card, CardItem, Body, Text, Spinner, Fab, Icon, Button } from 'native-base';
 const baseUrl = 'https://prabeen-restapi.herokuapp.com/api/posts';
 
 class Post extends Component {
@@ -7,10 +7,11 @@ class Post extends Component {
         super(props)
         this.state = {
             posts: [],
-            isFetching: false
+            isFetching: false,
+            active: 'true'
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             isFetching: true
         })
@@ -25,24 +26,48 @@ class Post extends Component {
             })
     }
     render() {
-        if(this.state.isFetching){
-            return <Text>Loading...</Text>
+        if (this.state.isFetching) {
+            return  <Spinner color='blue' />
         }
         return (
             <Container>
-            {this.state.posts.map(post => {
-                return(
-                    <Card>
-                    <CardItem>
-                      <Body>
-                        <Text>
-                           //Your text here
-                        </Text>
-                      </Body>
-                    </CardItem>
-                  </Card>
-                )
-            })}
+                <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{ }}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}>
+            <Icon name="share" />
+            <Button style={{ backgroundColor: '#34A34F' }}>
+              <Icon name="logo-whatsapp" />
+            </Button>
+            <Button style={{ backgroundColor: '#3B5998' }}>
+              <Icon name="logo-facebook" />
+            </Button>
+            <Button disabled style={{ backgroundColor: '#DD5144' }}>
+              <Icon name="mail" />
+            </Button>
+          </Fab>
+                {this.state.posts.map(post => {
+                    return (
+                            <Card>
+                                <CardItem header>
+                                    <Text>{post.title}</Text>
+                                </CardItem>
+                                <CardItem>
+                                    <Body>
+                                        <Text>
+                                            {post.body}
+                                        </Text>
+                                    </Body>
+                                </CardItem>
+                                <CardItem footer>
+                                    <Text>{post.author}: {post.date}</Text>
+                                </CardItem>
+                            </Card>
+                    )
+                })}
             </Container>
         )
     }
